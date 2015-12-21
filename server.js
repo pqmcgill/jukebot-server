ae0ea4c00ff2307f016694429a6384385fd76bc5
pqmcgill <Gocolts90!>
2015-12-21 12:29:51 -0600

    CORS

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+
+  if ('OPTIONS' === req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 
 var port = process.env.PORT || 3000;
