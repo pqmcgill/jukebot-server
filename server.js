0068e701925094ac93e46e035b201cb4e90d84e9
pqmcgill <Gocolts90!>
2015-12-21 13:01:39 -0600

    CORS

--- server.js
+++ server.js
@@ -20,42 +20,39 @@ var client_id = process.env.API_KEY;
 var client_secret = process.env.SECRET_KEY;
 
 // Routes
-app.post('/api/v1/rhapsody/auth', cors(), function(req, res) {
-  res.json({
-    returnedCode: req.body.code
-  });
-  //  var code = req.body.code;
-  //  if (code) {
-  //    console.log('CODE HERE:', code);
-  //    request.post({
-  //      url: 'https://api.rhapsody.com/oauth/access_token',
-  //      form: {
-  //        'client_id': client_id,
-  //        'client_secret': client_secret,
-  //        'response_type': 'code',
-  //        'code': code,
-  //        'redirect_uri': 'http://google.com',
-  //        'grant_type': 'authorization_code'
-  //      }
-  //    }, function(err, response, body) {
-  //      if (err) {
-  //        res.status(500);
-  //        res.json({
-  //          error: err,
-  //          status: 500,
-  //          msg: 'Error connecting to rhapsody auth service'
-  //        });
-  //      } else {
-  //        res.json(body);
-  //      }
-  //    });
-  //  } else {
-  //    res.status(400);
-  //    res.json({
-  //      status: 400,
-  //      msg: 'Bad Request: valid code required'
-  //    });
-  //  }
+app.post('/api/v1/rhapsody/auth', function(req, res) {
+   var code = req.body.code;
+   if (code) {
+     console.log('CODE HERE:', code);
+     request.post({
+       url: 'https://api.rhapsody.com/oauth/access_token',
+       form: {
+         'client_id': client_id,
+         'client_secret': client_secret,
+         'response_type': 'code',
+         'code': code,
+         'redirect_uri': 'http://google.com',
+         'grant_type': 'authorization_code'
+       }
+     }, function(err, response, body) {
+       if (err) {
+         res.status(500);
+         res.json({
+           error: err,
+           status: 500,
+           msg: 'Error connecting to rhapsody auth service'
+         });
+       } else {
+         res.json(body);
+       }
+     });
+   } else {
+     res.status(400);
+     res.json({
+       status: 400,
+       msg: 'Bad Request: valid code required'
+     });
+   }
 });
 
 // Run Server
